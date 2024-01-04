ad4e8c061132c217ebc8eccc725ecf41e49e4438
Jeremy <jeremysimwenze@gmail.com>
2024-01-04 18:11:51 +0800

    Create new table, added 4 values

--- create-tables.sql
+++ create-tables.sql
@@ -1,13 +1,56 @@
 DROP TABLE IF EXISTS album;
-CREATE TABLE album (
+// Example
+CREATE TABLE yourmom (
   id         SERIAL NOT NULL, 
   title      VARCHAR(128) NOT NULL,
   artist     VARCHAR(255) NOT NULL,
   price      DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (`id`)
 );
 
-INSERT INTO album
+
+// MAIN DATABASE SCHEMA
+
+CREATE TABLE users (
+  user_id      SERIAL PRIMARY KEY,
+  username     VARCHAR(50) NOT NULL UNIQUE,
+);
+
+CREATE TABLE posts (
+  post_id       SERIAL PRIMARY KEY,
+  title         VARCHAR(255) NOT NULL,
+  content       TEXT,
+  user_id       INT REFERENCES users(user_id) ON DELETE CASCADE,
+  created_at    TIMESTAMP DEFAULT current_timestamp
+
+);
+
+CREATE TABLE comment (
+  comment_id    SERIAL PRIMARY KEY,
+  content       TEXT NOT NULL,
+  user_id       INT REFERENCES users(user_id) ON DELETE CASCADE,
+  post_id       INT REFERENCES posts(post_id) ON DELETE CASCADE,
+  created_at    TIMESTAMP DEFAULT current_timestamp
+  reply_id      //search
+  
+);
+
+
+CREATE TABLE post_likes (
+  post_id       INT REFERENCES posts(post_id) ON DELETE CASCADE,
+  user_id       INT REFERENCES users(user_id) ON DELETE CASCADE,
+  
+);
+
+CREATE TABLE post_category (
+  category_id       SERIAL PRIMARY KEY,
+  post_id           INT REFERENCES posts(post_id) ON DELETE CASCADE,
+  
+);
+
+
+
+
+INSERT INTO yourmom
   (title, artist, price)
 VALUES
   ('Blue Train', 'John Coltrane', 56.99),
--- gin.go
+++ gin.go
@@ -25,11 +25,13 @@ type Album struct {
 
 func main() {
         // Capture connection properties.
-        username := os.Getenv("DBUSER")
-        password := os.Getenv("DBPASS")
-        album := os.Getenv("ALBUM")
+        // username := os.Getenv("DBUSER")
+        // password := os.Getenv("DBPASS")
+        // album := os.Getenv("ALBUM")
+        // connStr := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", username, album, password)
 
-        connStr := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", username, album, password)
+        connstr := fmt.Sprintf("user=album_lsio_user dbname=album_lsio password=SRBn2Lo0NGLoDS37C5I8AGkmsxbypjmm sslmode=disable")
+
 
         // Get a database handle.
         var err error
